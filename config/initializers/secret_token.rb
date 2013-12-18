b0d5cdac6b558b14c146c600cc9fb4f8f4bccdd7
kayhide <kayhide@gmail.com>
2013-12-18 17:14:45 +0900

    init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imadoko::Application.config.secret_key_base = '1d491a184333f8c51a35236ce33c64d576ae41ab5224b39d1c818ac5ec1771a6095ebddd23e73f9faa60e61c0d2b5dbbb205fb1da8f960c9929daef8f3d69442'
